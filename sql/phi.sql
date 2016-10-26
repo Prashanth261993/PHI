4ae7489af19a2f3b163a89a67fd6ff150efc8dec
alan.jeris@gmail.com <alan.jeris@gmail.com>
2016-10-26 16:44:15 -0400

    Added triggers to sqldump.

--- sql/phi.sql
+++ sql/phi.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.16, for Linux (x86_64)
+CREATE DATABASE  IF NOT EXISTS `phi` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `phi`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: phi
 -- ------------------------------------------------------
--- Server version	5.7.16-0ubuntu0.16.04.1
+-- Server version	5.7.15-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -120,9 +122,31 @@ CREATE TABLE `health_supporter` (
 
 LOCK TABLES `health_supporter` WRITE;
 /*!40000 ALTER TABLE `health_supporter` DISABLE KEYS */;
-INSERT INTO `health_supporter` VALUES (1,'Leonard Hofstader','Sacramento SC','9199145555',2),(2,'Penny Hofstader','2500-204 Sacramento','+1(919)9170067',3),(3,'Amy Farrahfowler','2500-204 Sacramento','+1(919)9170067',4);
+INSERT INTO `health_supporter` VALUES (1,'Leonard Hofstader','Sacramento SC','+1-9199170098',2),(2,'Penny Hofstader','2500-204 Sacramento','+1-9199170098',3),(3,'Amy Farrahfowler','2500-204 Sacramento','+1-9199170098',4);
 /*!40000 ALTER TABLE `health_supporter` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER hs_phone_check BEFORE INSERT ON health_supporter
+FOR EACH ROW 
+BEGIN 
+IF (NEW.phone_num REGEXP '^(\\+?[0-9]{1,4}-)?[0-9]{3,10}$' ) = 0 THEN 
+  SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Wrong phone number format!!!';
+END IF; 
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `hs_manages_patient`
@@ -153,6 +177,99 @@ LOCK TABLES `hs_manages_patient` WRITE;
 INSERT INTO `hs_manages_patient` VALUES (2,2,1,'family','2016-10-09 00:00:00'),(3,3,1,'family','2016-10-21 00:00:00'),(1,1,0,'social worker','2016-10-22 00:00:00'),(1,3,1,'family','2016-10-22 00:00:00');
 /*!40000 ALTER TABLE `hs_manages_patient` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER pat_hs_primary_ind_check BEFORE INSERT ON hs_manages_patient
+FOR EACH ROW 
+BEGIN 
+IF NEW.primary_ind NOT IN (0,1) THEN 
+  SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Invalid primary relation(should be either 0 or 1 only)!!!';
+END IF; 
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER pat_hs_relation_check BEFORE INSERT ON hs_manages_patient
+FOR EACH ROW 
+BEGIN 
+IF NEW.relation NOT IN ('family','friend','social worker') THEN 
+  SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Invalid relation between patient and health supporter!!!';
+END IF; 
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER patient_cant_add_self_as_hs_check BEFORE INSERT ON hs_manages_patient
+FOR EACH ROW 
+BEGIN 
+
+IF (select count(*) from health_supporter hs, patient p, user_info u where hs.user_id = u.id and p.user_id = u.id and p.id = NEW.p_id and hs.id=NEW.hs_id) > 0 THEN
+  SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Patient cannnot add self as a health supporter!!!';
+END IF; 
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER sick_patient_hs_count_check BEFORE DELETE ON hs_manages_patient
+FOR EACH ROW 
+BEGIN 
+
+declare hs_count int;
+set hs_count = (select count(*) from hs_manages_patient hsp where hsp.p_id = OLD.p_id);
+
+IF hs_count < 2 and (select count(*) from patient_diagnosis pd where pd.p_id = OLD.p_id) > 0 THEN
+  SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Sick patient should have atleast one health supporter!!!';
+END IF; 
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `mood`
@@ -197,7 +314,7 @@ CREATE TABLE `observation` (
   KEY `pid` (`pid`),
   CONSTRAINT `observation_ibfk_1` FOREIGN KEY (`observation_type_id`) REFERENCES `observation_type` (`observation_type_id`),
   CONSTRAINT `observation_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `patient` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -206,7 +323,7 @@ CREATE TABLE `observation` (
 
 LOCK TABLES `observation` WRITE;
 /*!40000 ALTER TABLE `observation` DISABLE KEYS */;
-INSERT INTO `observation` VALUES (1,1,2,'2016-10-10 00:00:00','2016-10-11 00:00:00'),(2,1,2,'2016-10-17 00:00:00','2016-10-17 00:00:00'),(3,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(4,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(5,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(6,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(7,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(8,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(9,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(10,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(11,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(12,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(13,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(14,5,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(15,5,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(16,5,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(17,6,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(18,6,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(19,3,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(20,3,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(21,3,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(22,4,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(23,3,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(24,4,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(25,4,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(26,6,1,'2016-01-01 00:00:00','2016-10-25 00:00:00');
+INSERT INTO `observation` VALUES (1,1,2,'2016-10-10 00:00:00','2016-10-11 00:00:00'),(2,1,2,'2016-10-17 00:00:00','2016-10-17 00:00:00'),(3,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(4,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(5,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(6,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(7,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(8,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(9,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(10,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(11,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(12,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(13,1,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(14,5,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(15,5,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(16,5,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(17,6,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(18,6,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(19,3,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(20,3,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(21,3,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(22,4,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(23,3,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(24,4,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(25,4,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(26,6,1,'2016-01-01 00:00:00','2016-10-25 00:00:00'),(27,1,1,'2015-01-12 00:00:00','2016-10-26 00:00:00');
 /*!40000 ALTER TABLE `observation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -252,6 +369,28 @@ LOCK TABLES `observation_requirement` WRITE;
 INSERT INTO `observation_requirement` VALUES (1,1,NULL,NULL,1,120,200,NULL,1,7,1),(2,1,NULL,NULL,1,140,159,NULL,1,1,1),(2,2,NULL,NULL,1,90,99,NULL,1,1,1),(5,5,NULL,NULL,1,1,1,NULL,1,1,1),(1,1,NULL,NULL,2,120,200,NULL,1,7,1),(2,1,NULL,NULL,2,NULL,NULL,NULL,1,1,1),(2,2,NULL,NULL,2,NULL,NULL,NULL,1,1,1),(4,4,NULL,NULL,2,1,5,NULL,1,1,1),(3,3,NULL,NULL,7,90,99,NULL,1,1,1),(6,6,NULL,NULL,7,95,100,NULL,1,1,1),(1,1,NULL,NULL,0,120,200,NULL,0,7,1),(1,1,2,2,2,120,190,'2016-10-10 00:00:00',1,7,1),(2,1,2,2,2,NULL,NULL,'2016-10-10 00:00:00',1,1,1),(2,2,2,2,2,NULL,NULL,'2016-10-10 00:00:00',1,1,1),(4,4,2,2,2,1,5,'2016-10-10 00:00:00',1,1,1);
 /*!40000 ALTER TABLE `observation_requirement` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER observation_requirement_freq_check BEFORE INSERT ON observation_requirement
+FOR EACH ROW 
+BEGIN 
+IF NEW.frequency < 1 THEN 
+  SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Frequency cannot be less than 1 day!!!';
+END IF; 
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `observation_sub_type`
@@ -385,9 +524,53 @@ CREATE TABLE `patient` (
 
 LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
-INSERT INTO `patient` VALUES (1,'P1','1984-05-05','New Jersey NY','male','919917067',1),(2,'Leonard Hofstader','0000-00-00','Chapel Hill RTP','male','+1(917)9190045',2),(3,'Penny Hofstader','1989-12-25','2500-204 Sacramento','female','+1(919)9170067',3),(4,'Amy Farrahfowler','1989-06-15','2500-204 Sacramento','female','+1(919)9170067',4);
+INSERT INTO `patient` VALUES (1,'P1','1984-05-05','New Jersey NY','male','+1-9199170098',1),(2,'Leonard Hofstader','1980-10-22','Chapel Hill RTP','male','+1-9199170098',2),(3,'Penny Hofstader','1989-12-25','2500-204 Sacramento','female','+1-9199170098',3),(4,'Amy Farrahfowler','1989-06-15','2500-204 Sacramento','female','+1-9199170098',4);
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER pat_gender BEFORE INSERT ON patient
+FOR EACH ROW 
+BEGIN 
+IF NEW.gender NOT IN ('male','female','others') THEN 
+  SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Invalid gender!!!';
+END IF; 
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp850 */ ;
+/*!50003 SET character_set_results = cp850 */ ;
+/*!50003 SET collation_connection  = cp850_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER pat_phone_check BEFORE INSERT ON patient
+FOR EACH ROW 
+BEGIN 
+IF (NEW.phone_num REGEXP '^(\\+?[0-9]{1,4}-)?[0-9]{3,10}$' ) = 0 THEN 
+  SIGNAL SQLSTATE '12345'
+     SET MESSAGE_TEXT = 'Wrong phone number format!!!';
+END IF; 
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `patient_diagnosis`
@@ -555,7 +738,7 @@ CREATE TABLE `weight` (
 
 LOCK TABLES `weight` WRITE;
 /*!40000 ALTER TABLE `weight` DISABLE KEYS */;
-INSERT INTO `weight` VALUES (180,1),(195,2),(2,12),(50,13);
+INSERT INTO `weight` VALUES (180,1),(195,2),(2,12),(50,13),(74,27);
 /*!40000 ALTER TABLE `weight` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -659,4 +842,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-26 14:06:59
+-- Dump completed on 2016-10-26 16:40:08
